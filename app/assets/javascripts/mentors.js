b62be1a8c991c8f8599c9ca59bbeaa4ad23898be
franklingu <franklingujunchao@gmail.com>
2015-04-17 00:10:11 +0800

    added front end validation for forms
    submission form is not validated yet though, waiting on #50
    fixes #42

--- app/assets/javascripts/mentors.js
+++ app/assets/javascripts/mentors.js
@@ -1,5 +1,5 @@
 //= require validator
 'use strict';
 $(document).ready(function () {
-  $('form[id^=edit_student], form[id^=new_student]').validator();
+    $('form[id^=edit_mentor], form[id^=new_mentor]').validator();
 });
--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,5 +1,5 @@
 //= require validator
 'use strict';
 $(document).ready(function () {
-  $('form[id^=edit_student], form[id^=new_student]').validator();
+    $('form[id^=edit_team], form[id^=new_team]').validator();
 });
