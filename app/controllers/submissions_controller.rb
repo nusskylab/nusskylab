5ab658095e124bb463552ca10fa47e93b0afbde6
franklingu <franklingujunchao@gmail.com>
2015-04-25 15:47:19 +0800

    display error msgs to user, at least for submission
    styling of error msgs can be improved of course
    Close #53 for now, open new one for other forms

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,42 +1,79 @@
 class SubmissionsController < ApplicationController
   def index
+    @submissions = Submission.all
   end
 
   def new
     @submission = Submission.new(team_id: params[:team_id])
-    render locals: {
-             milestones: Milestone.all
-           }
+    render_new_action and return
   end
 
   def create
-    @submission = create_or_update_submission
-    redirect_to team_submission_path(@submission.team_id, @submission.id)
+    sub = create_submission
+    render_or_redirect_for_submission(sub, true)
   end
 
   def show
     @submission = Submission.find(params[:id])
-    render locals: {
-             milestones: Milestone.all
-           }
   end
 
   def edit
     @submission = Submission.find(params[:id])
-    render locals: {
-             milestones: Milestone.all
-           }
+    render_edit_action and return
   end
 
   def update
-    @submission = create_or_update_submission
-    redirect_to team_submission_path(@submission.team_id, @submission.id)
+    sub = update_submission
+    render_or_redirect_for_submission(sub, false)
   end
 
   private
-  def create_or_update_submission
+  def create_submission
+    sub_params = exact_submission_params
+    @submission = Submission.new(sub_params)
+    @submission.save ? @submission : nil
+  end
+
+  def update_submission
+    sub_params = exact_submission_params
+    @submission = Submission.find(params[:id])
+    if not (@submission.milestone_id == sub_params[:milestone_id].to_i)
+      @submission.errors.add(:milestone_id,
+                             'You cannot change milestone id once submitted, use new instead')
+      return nil
+    elsif not (@submission.team_id == sub_params[:team_id].to_i)
+      @submission.errors.add(:team_id,
+                             'You cannot change team id once submitted, use new instead')
+      return nil
+    end
+    @submission.update(sub_params) ? @submission : nil
+  end
+
+  def exact_submission_params
     submission_params = params.require(:submission).permit(:milestone_id, :read_me, :project_log, :video_link)
     submission_params[:team_id] = params[:team_id]
-    Submission.create_or_update_by_team_id_and_milestone_id(submission_params)
+    submission_params
+  end
+
+  def render_or_redirect_for_submission(sub, is_create_action)
+    if sub
+      redirect_to team_submission_path(@submission.team_id, @submission.id)
+    elsif is_create_action
+      render_new_action
+    else
+      render_edit_action
+    end
+  end
+
+  def render_new_action
+    render 'new', locals: {
+             milestones: Milestone.all
+                }
+  end
+
+  def render_edit_action
+    render 'edit', locals: {
+             milestones: Milestone.all
+                 }
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,21 +3,8 @@ class Submission < ActiveRecord::Base
   validates :team_id, presence: true,
             uniqueness: {scope: :milestone_id,
                          message: 'A team can only submit one copy to every milestone'}
+  validates :project_log, :read_me, :video_link, presence: true
 
   belongs_to :milestone
   belongs_to :team
-
-  validates :milestone_id, :team_id, :project_log, :read_me, :video_link, :presence => true
-
-  def self.create_or_update_by_team_id_and_milestone_id(submission_hash)
-    submission = Submission.find_by(team_id: submission_hash[:team_id],
-                                    milestone_id: submission_hash[:milestone_id]) || Submission.new
-    submission_hash.each_pair do |key, value|
-      if submission.has_attribute?(key) and (not value.blank?)
-        submission[key] = value
-      end
-    end
-    submission.save
-    return submission
-  end
 end
