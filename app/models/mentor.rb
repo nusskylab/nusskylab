589e7d122954433e103a6ef0488a0cf8540b2e69
franklingu <franklingujunchao@gmail.com>
2015-05-04 21:42:17 +0800

    checked all actions for mentor
    Close #88

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,21 +1,11 @@
 class Mentor < ActiveRecord::Base
-  validates :user_id, presence: true
+  validates :user_id, presence: true,
+            uniqueness: {message: 'can only have one admin role'}
 
   belongs_to :user
   has_many :teams
 
   def self.mentor?(user_id)
     Mentor.find_by(user_id: user_id)
-  end
-
-  def self.create_or_update_by_user_id(mentor_hash)
-    mentor = Mentor.find_by(user_id: mentor_hash[:user_id]) || Mentor.new
-    mentor_hash.each_pair do |key, value|
-      if mentor.has_attribute?(key) and (not value.blank?)
-        mentor[key] = value
-      end
-    end
-    mentor.save
-    return mentor
   end
 end
