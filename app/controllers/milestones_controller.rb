43cc855bef1be83f5f6232e7283fa99b39391d4a
franklingu <franklingujunchao@gmail.com>
2015-05-05 21:05:27 +0800

    complete actions in milestone controller
    Close #92

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -8,18 +8,46 @@ class MilestonesController < ApplicationController
   end
 
   def create
-    # TODO: find a more elegant way of creating date. besides, the front end presentation looks like crap
-    test = Milestone.new(params.require(:milestone).permit(:name, :deadline))
-    @milestone = Milestone.create_or_update_by_name(:name => test.name, :deadline => test.deadline)
-    redirect_to milestones_path
+    test = Milestone.new(get_milestone_params)
+    @milestone = Milestone.new(:name => test.name, :deadline => test.deadline)
+    if @milestone.save
+      flash = {}
+      flash[:success] = 'The milestone is successfully created'
+      redirect_to milestones_path, flash: flash
+    else
+      render 'new'
+    end
   end
 
   def show
+    @milestone = Milestone.find(params[:id])
+  end
+
+  def edit
+    @milestone = Milestone.find(params[:id])
   end
 
   def update
+    @milestone = Milestone.find(params[:id])
+    if @milestone.update(get_milestone_params)
+      flash = {}
+      flash[:success] = 'The milestone is successfully edited'
+      redirect_to milestones_path, flash: flash
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @milestone = Milestone.find(params[:id])
+    @milestone.destroy
+    flash = {}
+    flash[:success] = 'The milestone is successfully deleted'
+    redirect_to milestones_path, flash: flash
   end
+
+  private
+    def get_milestone_params
+      params.require(:milestone).permit(:name, :deadline)
+    end
 end
