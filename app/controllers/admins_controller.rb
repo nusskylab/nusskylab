56dc5c122ee00ba59596a636f4663d2a2e9de8ee
franklingu <franklingujunchao@gmail.com>
2015-05-07 21:07:39 +0800

    fix exception when creating admin action returns error
    Close #94

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,6 +6,7 @@ class AdminsController < ApplicationController
   def new
     @admin = Admin.new
     render locals: {
+             user: nil,
              users: User.all
            }
   end
@@ -42,54 +43,56 @@ class AdminsController < ApplicationController
   end
 
   private
-  def get_user_param
-    user_param = params.require(:user).permit(:user_name, :email, :uid, :provider)
-  end
-
-  def create_user_and_admin
-    # TODO: handle errors here more gracefully
-    user_params = get_user_param
-    user = User.new(user_params)
-    if user.save
-      @admin = Admin.new(user_id: user.id)
-      if @admin.save
-        redirect_to admins_path
-      else
-        render 'new', locals: {
-                 users: User.all
-               }
-      end
-    else
-      render 'new', locals: {
-                    users: User.all
-                  }
+    def get_user_param
+      user_param = params.require(:user).permit(:user_name, :email, :uid, :provider)
     end
-  end
 
-  def create_admin_using_existing_user
-    # TODO: handle errors here more gracefully
-    user = User.find(params[:admin][:user_id])
-    if user
-      @admin = Admin.new(user_id: user.id)
-      if @admin.save
-        redirect_to admins_path
+    def create_user_and_admin
+      user_params = get_user_param
+      user = User.new(user_params)
+      if user.save
+        @admin = Admin.new(user_id: user.id)
+        if @admin.save
+          redirect_to admins_path
+        else
+          render 'new', locals: {
+                   user: user,
+                   users: User.all
+                 }
+        end
       else
         render 'new', locals: {
+                      user: user,
                       users: User.all
                     }
       end
-    else
-      render 'new', locals: {
-                    users: User.all
-                  }
     end
-  end
 
-  def update_user
-    user = @admin.user
-    user_param = get_user_param
-    user_param[:uid] = user.uid
-    user_param[:provider] = user.provider
-    user.update(user_param) ? user : nil
-  end
+    def create_admin_using_existing_user
+      user = User.find(params[:admin][:user_id])
+      if user
+        @admin = Admin.new(user_id: user.id)
+        if @admin.save
+          redirect_to admins_path
+        else
+          render 'new', locals: {
+                        user: user,
+                        users: User.all
+                      }
+        end
+      else
+        render 'new', locals: {
+                      user: user,
+                      users: User.all
+                    }
+      end
+    end
+
+    def update_user
+      user = @admin.user
+      user_param = get_user_param
+      user_param[:uid] = user.uid
+      user_param[:provider] = user.provider
+      user.update(user_param) ? user : nil
+    end
 end
