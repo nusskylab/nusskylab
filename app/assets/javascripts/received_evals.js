df135d3d88521ce6252bdee2006afccad71bb038
franklingu <franklingujunchao@gmail.com>
2015-05-19 22:02:45 +0800

    add js support for received evals view
    Close #102

--- app/assets/javascripts/received_evals.js
+++ app/assets/javascripts/received_evals.js
@@ -1,3 +1,29 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+'use strict';
+$(document).ready(function () {
+  $('#public-content > div > .eval-public-value').each(function (idx, itm) {
+    fillEvalHtmlForm(itm, '.eval-public-part');
+  });
+  $('#private-content > #private-part-compiled > div > .eval-private-value').each(function (idx, itm) {
+    fillEvalHtmlForm(itm, '.eval-private-part');
+  });
+
+  disableAllEvalFormInputs();
+
+  function fillEvalHtmlForm(itm, formClass) {
+    try {
+      var publicValues = JSON.parse($(itm).html());
+      $.each(publicValues, function (attrName, attrVal) {
+        var radioSelector = formClass + ' > div input[type=radio][name="' + attrName + '"][value="' + attrVal + '"]';
+        $(itm).parent().find(radioSelector).attr('checked', 'checked');
+        var textareaSlector = formClass + ' > div textarea[name="' + attrName + '"]';
+        $(textareaSlector).val(attrVal);
+      });
+    } catch (err) {
+    }
+  }
+
+  function disableAllEvalFormInputs() {
+    $('input').attr('disabled', 'disabled');
+    $('textarea').attr('disabled', 'disabled');
+  }
+});
