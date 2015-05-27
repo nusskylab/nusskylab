2876863d111e06d9ac8e80af127e863d32d9bd54
franklingu <franklingujunchao@gmail.com>
2015-05-27 23:35:02 +0800

    action mailer not configured yet--not working

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -1,23 +1,25 @@
 class Auth::PasswordsController < Devise::PasswordsController
+  layout 'general_layout'
+
   # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # protected
 
