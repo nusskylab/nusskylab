af8bcb3fe0e8ae5431a654db01c83fca742e36fc
franklingu <franklingujunchao@gmail.com>
2015-05-28 22:50:51 +0800

    enabled mailer with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,18 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'http://nusskylab-mht7cvdwt2.elasticbeanstalk.com/'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :user_name => ENV['SENDGRID_USER_NAME'],
+    :password => ENV['SENDDRID_USER_PASSWORD'],
+    :domain => 'nusskylab-mht7cvdwt2.elasticbeanstalk.com/',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
