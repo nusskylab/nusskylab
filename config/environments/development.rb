15fbd08e1b5cf0b1dee80493269a5f2b5eb66f71
franklingu <franklingujunchao@gmail.com>
2015-05-31 13:38:25 +0800

    add js validation for peer-evals
    Close #134

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
