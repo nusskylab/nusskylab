b1f0d319d86ac4c595a50f2e22c06281baa29f40
franklingu <franklingujunchao@gmail.com>
2015-06-01 10:12:07 +0800

    change evaluator uniqueness constraint
    Close #156

--- app/models/evaluating.rb
+++ app/models/evaluating.rb
@@ -1,7 +1,7 @@
 class Evaluating < ActiveRecord::Base
   validates :evaluated_id, presence: true
   validates :evaluator_id, presence: true,
-            uniqueness: {message: 'evaluating relationship should be unique'}
+            uniqueness: {scope: :evaluated_id, message: 'evaluating relationship should be unique'}
 
   belongs_to :evaluator, foreign_key: :evaluator_id, class_name: Team
   belongs_to :evaluated, foreign_key: :evaluated_id, class_name: Team
