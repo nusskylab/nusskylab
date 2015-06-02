ac5899e155acdebcc3191e5da35e5d91cd4c45da
franklingu <franklingujunchao@gmail.com>
2015-06-02 21:37:59 +0800

    added sort feature to evaluatings table
    Close #163
    may consider just include sortable table js in application js instead

--- app/assets/javascripts/evaluatings.js
+++ app/assets/javascripts/evaluatings.js
@@ -7,3 +7,19 @@ $(document).ready(function () {
     width: '100%'
   });
 });
+
+$(function(){
+  if ($('.table-sortable').length) {
+    $('.table-sortable').tablesorter({
+      theme : 'bootstrap',
+      widthFixed: true,
+      headerTemplate : '{content} {icon}',
+      widgets : [ 'uitheme' ],
+      headers: {
+        '.unsortable' : {
+          sorter: false
+        }
+      }
+    })
+  }
+});
