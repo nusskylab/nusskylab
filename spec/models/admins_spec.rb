09bddceaeaae2329a76faa4d153ad2efb2aad9f8
franklingu <franklingujunchao@gmail.com>
2015-06-04 23:11:11 +0800

    add tests for admin models

---
+++ spec/models/admins_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Adviser do
+  it 'is invalid with required field missing' do
+    expect(FactoryGirl.build(:admin, user: nil)).not_to be_valid
+  end
+
+  it 'is invalid to associate with adviser#user' do
+    user = FactoryGirl.create(:user)
+    expect(FactoryGirl.create(:admin, user: user)).to be_valid
+    expect(FactoryGirl.build(:admin, user: user)).not_to be_valid
+  end
+end
