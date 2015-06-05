4ae32a85f39f84b82420724d308f843c5e070a4e
franklingu <franklingujunchao@gmail.com>
2015-06-05 22:12:55 +0800

    refactored js for common tablesorter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,19 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require select2
+
+$(function(){
+  if ($('.table-sortable').length) {
+    $('.table-sortable').tablesorter({
+      theme : 'bootstrap',
+      widthFixed: true,
+      headerTemplate : '{content} {icon}',
+      widgets : [ 'uitheme' ],
+      headers: {
+        '.unsortable' : {
+          sorter: false
+        }
+      }
+    });
+  }
+});
