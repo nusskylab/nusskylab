4ba492e0487131b2073897abced4eb85450bdc6f
franklingu <franklingujunchao@gmail.com>
2015-08-12 23:07:38 +0800

    fix edit team error for advisers and hide edit when approriate
    Close #229

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,14 @@
 module TeamsHelper
+  def can_edit_team
+    loggedin_user = current_user
+    if loggedin_user.nil?
+      return false
+    end
+    @team.get_relevant_users.each do |user|
+      if user.id == loggedin_user.id
+        return true
+      end
+    end
+    return false
+  end
 end
