6f9b28da6aaf8e879cc6a5d42769a706dbaf2eb4
franklingu <franklingujunchao@gmail.com>
2015-08-23 20:24:08 +0800

    added access control for peer evaluations and submissions

--- app/models/adviser.rb
+++ app/models/adviser.rb
@@ -8,4 +8,12 @@ class Adviser < ActiveRecord::Base
   def self.adviser?(user_id)
     Adviser.find_by(user_id: user_id)
   end
+
+  def get_advisee_users
+    advisee_users = []
+    self.teams.each do |team|
+      advisee_users.concat(team.get_team_members)
+    end
+    return advisee_users
+  end
 end
