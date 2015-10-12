0e5a7d13e897e8f40f2635709d7a9f5410870535
franklingu <franklingujunchao@gmail.com>
2015-10-12 23:20:59 +0800

    added a back to top button when page is long
    Close #291

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,3 +92,16 @@ $(function() {
 $(function() {
   autosize($('textarea:not(.tinymce)'));
 });
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {
+    $('#return-to-top').fadeIn(200);
+  } else {
+    $('#return-to-top').fadeOut(200);
+  }
+});
+$('#return-to-top').click(function() {
+  $('body,html').animate({
+    scrollTop : 0
+  }, 500);
+});
