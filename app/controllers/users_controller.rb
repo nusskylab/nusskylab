968cf906c08326addd1eb210da581e04c6e41eff
franklingu <franklingujunchao@gmail.com>
2015-12-10 14:51:24 +0800

    refactored users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,76 +1,98 @@
+# UsersController: manage actions related to user
+#   index:      list of users
+#   new:        view to create a user
+#   create:     create a user
+#   preview_as: for admin to login as another user
+#   edit:       view to update a user
+#   update:     update a user
+#   destroy:    delete a user
 class UsersController < ApplicationController
   def index
-    not authenticate_user(true, true) and return
+    !authenticate_user(true, true) && return
     @page_title = t('.page_title')
     @users = User.order(:user_name).all
   end
 
   def new
-    not authenticate_user(true, true) and return
+    !authenticate_user(true, true) && return
     @page_title = t('.page_title')
     @user = User.new
   end
 
   def create
-    not authenticate_user(true, true) and return
-    @user = User.new(get_user_params)
+    !authenticate_user(true, true) && return
+    @user = User.new(user_params(true))
     if @user.save
-      redirect_to users_path, flash: {success: t('.success_message')}
+      redirect_to users_path, flash: {
+        success: t('.success_message')
+      }
     else
-      redirect_to new_user_path, flash: {danger: t('.failure_message',
-                                                   error_message: @user.errors.full_messages.join(', '))}
+      redirect_to new_user_path, flash: {
+        danger: t('.failure_message',
+                  error_message: @user.errors.full_messages.join(', '))
+      }
     end
   end
 
   def preview_as
-    not authenticate_user(true, true) and return
-    user = User.find(params[:id])
+    !authenticate_user(true, true) && return
+    user = User.find(params[:id]) || (record_not_found && return)
     sign_out(current_user)
     sign_in(user)
     redirect_to user_path(user.id)
   end
 
+  def show
+    @user = User.find(params[:id]) || (record_not_found && return)
+    !authenticate_user(true, false, [@user]) && return
+    @page_title = t('.page_title', user_name: @user.user_name)
+  end
+
   def edit
-    @user = User.find(params[:id])
-    not authenticate_user(true, false, [@user]) and return
+    @user = User.find(params[:id]) || (record_not_found && return)
+    !authenticate_user(true, false, [@user]) && return
     @page_title = t('.page_title', user_name: @user.user_name)
   end
 
   def update
-    @user = User.find(params[:id])
-    not authenticate_user(true, false, [@user]) and return
-    user_params = get_user_params
-    user_params.except!(:provider)
-    if @user and @user.update(user_params)
-      redirect_to @user, flash: {success: t('.success_message')}
+    @user = User.find(params[:id]) || (record_not_found && return)
+    !authenticate_user(true, false, [@user]) && return
+    user_ps = user_params
+    user_ps.except!(:provider)
+    if @user.update(user_ps)
+      redirect_to @user, flash: {
+        success: t('.success_message')
+      }
     else
-      redirect_to @user, flash: {danger: t('.failure_message',
-                                           error_message: @user.errors.full_messages.join(', '))}
+      redirect_to @user, flash: {
+        danger: t('.failure_message',
+                  error_message: @user.errors.full_messages.join(', '))
+      }
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-    not authenticate_user(true, false, [@user]) and return
-    @page_title = t('.page_title', user_name: @user.user_name)
-  end
-
   def destroy
-    not authenticate_user(true, true) and return
+    !authenticate_user(true, true) && return
     @user = User.find(params[:id])
     if @user.destroy
-      redirect_to users_path, flash: {success: t('.success_message')}
+      redirect_to users_path, flash: {
+        success: t('.success_message')
+      }
     else
-      redirect_to users_path, flash: {danger: t('.failure_message')}
+      redirect_to users_path, flash: {
+        danger: t('.failure_message',
+                  error_message: @user.errors.full_messages.join(', '))
+      }
     end
   end
 
   private
-  def get_user_params
-    user_params = params.require(:user).permit(:user_name, :email, :uid, :provider)
-    generated_password = Devise.friendly_token.first(8)
-    user_params[:password] = generated_password
-    user_params[:provider] = user_params[:provider].to_i
-    user_params
+
+  def user_params(generate_pswd = false)
+    user_ps = params.require(:user).permit(:user_name, :email, :uid,
+                                           :provider)
+    user_ps[:password] = Devise.friendly_token.first(8) if generate_pswd
+    user_ps[:provider] = user_ps[:provider].to_i
+    user_ps
   end
 end
