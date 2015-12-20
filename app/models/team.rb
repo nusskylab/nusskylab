738911742827ea8a177a4278f6a2144799041018
franklingu <franklingujunchao@gmail.com>
2015-12-20 17:47:00 +0800

    added dependent delete and fixed confirm msgs
    Close #251

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,12 +4,12 @@ class Team < ActiveRecord::Base
     message: ': Team name should be unique'
   }
 
-  has_many :students
   belongs_to :adviser
   belongs_to :mentor
-  has_many :submissions
-  has_many :peer_evaluations
-  has_many :feedbacks
+  has_many :students, dependent: :destroy
+  has_many :submissions, dependent: :destroy
+  has_many :peer_evaluations, dependent: :destroy
+  has_many :feedbacks, dependent: :destroy
   has_many :evaluateds, class_name: :Evaluating, foreign_key: :evaluator_id
   has_many :evaluators, class_name: :Evaluating, foreign_key: :evaluated_id
 
