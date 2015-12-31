2f97a71073d8deb28c7ecefd7196141feeb095ae
franklingu <franklingujunchao@gmail.com>
2016-01-01 00:02:06 +0800

    refactored evaluatings and feedbacks controller

--- app/controllers/evaluatings_controller.rb
+++ app/controllers/evaluatings_controller.rb
@@ -12,13 +12,13 @@ class EvaluatingsController < ApplicationController
     elsif current_user_adviser?
       @evaluatings = Adviser.adviser?(current_user.id).advised_teams_evaluatings
     else
-      raise ActionController::RoutingError.new(t('application.path_not_found_message'))
+      fail ActionController::RoutingError, t('application.path_not_found_message')
     end
     @page_title = t('.page_title')
   end
 
   def new
-    !authenticate_user(true, false, Adviser.all.map{|adviser| adviser.user}) && return
+    !authenticate_user(true, false, Adviser.all.map(&:user)) && return
     @evaluating = Evaluating.new
     adviser = Adviser.adviser?(current_user.id)
     if current_user_admin?
@@ -27,74 +27,80 @@ class EvaluatingsController < ApplicationController
       teams = adviser.teams
     end
     @page_title = t('.page_title')
-    render locals: {teams: teams}
+    render locals: { teams: teams }
   end
 
   def create
-    !authenticate_user(true, false, Adviser.all.map{|adviser| adviser.user}) && return
-    @evaluating = Evaluating.new(get_evaluating_params)
+    !authenticate_user(true, false, Adviser.all.map(&:user)) && return
+    @evaluating = Evaluating.new(evaluating_params)
     if create_or_update_evaluation_relationship
-      redirect_to evaluatings_path,
-                  flash: {success: t('.success_message',
-                                     entity1_name: @evaluating.evaluator.team_name,
-                                     entity2_name: @evaluating.evaluated.team_name)}
+      redirect_to evaluatings_path, flash: {
+        success: t('.success_message',
+                   entity1_name: @evaluating.evaluator.team_name,
+                   entity2_name: @evaluating.evaluated.team_name)
+      }
     else
-      redirect_to new_evaluating_path,
-                  flash: {danger: t('.failure_message',
-                                    error_message: @evaluating.errors.full_messages.join(', '))}
+      redirect_to new_evaluating_path, flash: {
+        danger: t('.failure_message',
+                  error_message: @evaluating.errors.full_messages.join(', '))
+      }
     end
   end
 
   def edit
     @evaluating = Evaluating.find(params[:id])
-    !authenticate_user(true, false, get_evaluating_permitted_users) && return
+    !authenticate_user(true, false, evaluating_permitted_users) && return
     if current_user_admin?
       teams = Team.all
     elsif (adviser = current_user_adviser?)
       teams = adviser.teams
     end
     @page_title = t('.page_title')
-    render locals: {teams: teams}
+    render locals: { teams: teams }
   end
 
   def update
     @evaluating = Evaluating.find(params[:id])
-    !authenticate_user(true, false, get_evaluating_permitted_users) && return
-    @evaluating.assign_attributes(get_evaluating_params)
+    !authenticate_user(true, false, evaluating_permitted_users) && return
+    @evaluating.assign_attributes(evaluating_params)
     if create_or_update_evaluation_relationship
-      redirect_to evaluatings_path,
-                  flash: {success: t('.success_message',
-                                     entity1_name: @evaluating.evaluator.team_name,
-                                     entity2_name: @evaluating.evaluated.team_name)}
+      redirect_to evaluatings_path, flash: {
+        success: t('.success_message',
+                   entity1_name: @evaluating.evaluator.team_name,
+                   entity2_name: @evaluating.evaluated.team_name)
+      }
     else
-      redirect_to edit_evaluating_path(@evaluating),
-                  flash: {danger: t('.failure_message',
-                                    error_message: @evaluating.errors.full_messages.join(', '))}
+      redirect_to edit_evaluating_path(@evaluating), flash: {
+        danger: t('.failure_message',
+                  error_message: @evaluating.errors.full_messages.join(', '))
+      }
     end
   end
 
   def destroy
     @evaluating = Evaluating.find(params[:id])
-    !authenticate_user(true, false, get_evaluating_permitted_users) && return
+    !authenticate_user(true, false, evaluating_permitted_users) && return
     if @evaluating.destroy
-      redirect_to evaluatings_path,
-                  flash: {success: t('.success_message',
-                                     entity1_name: @evaluating.evaluator.team_name,
-                                     entity2_name: @evaluating.evaluated.team_name)}
+      redirect_to evaluatings_path, flash: {
+        success: t('.success_message',
+                   entity1_name: @evaluating.evaluator.team_name,
+                   entity2_name: @evaluating.evaluated.team_name)
+      }
     else
-      redirect_to evaluatings_path,
-                  flash: {danger: t('.failure_message',
-                                    error_message: @evaluating.errors.full_messages.join(', '))}
+      redirect_to evaluatings_path, flash: {
+        danger: t('.failure_message',
+                  error_message: @evaluating.errors.full_messages.join(', '))
+      }
     end
   end
 
   private
 
-  def get_evaluating_params
+  def evaluating_params
     params.require(:evaluating).permit(:evaluated_id, :evaluator_id)
   end
 
-  def get_evaluating_permitted_users
+  def evaluating_permitted_users
     evaluating_users = []
     adviser = Adviser.adviser?(current_user.id) if current_user
     if adviser && @evaluating.evaluated.adviser_id == adviser.id
@@ -107,9 +113,11 @@ class EvaluatingsController < ApplicationController
     adviser = Adviser.adviser?(current_user.id)
     if current_user_admin?
       return @evaluating.save ? @evaluating : nil
-    elsif not adviser.nil?
-      if (@evaluating.evaluated and @evaluating.evaluated.adviser_id == adviser.id) and
-        (@evaluating.evaluator and @evaluating.evaluator.adviser_id == adviser.id)
+    elsif !adviser.nil?
+      if (@evaluating.evaluated &&
+          @evaluating.evaluated.adviser_id == adviser.id) &&
+         (@evaluating.evaluator &&
+          @evaluating.evaluator.adviser_id == adviser.id)
         return @evaluating.save ? @evaluating : nil
       end
     end
--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,12 @@
+# FeedbackController: manage actions related to feedbacks
+#   new:    view to create a feedback
+#   create: create a feedback
+#   edit:   view to edit a feedback
+#   update: update a feedback
 class FeedbacksController < ApplicationController
   def new
     team = Team.find(params[:team_id])
-    not authenticate_user(true, false, team.get_relevant_users(false, false)) and return
+    !authenticate_user(true, false, team.get_relevant_users(false, false)) && return
     @page_title = t('.page_title')
     @feedback = Feedback.new
     evaluators = []
@@ -9,17 +14,17 @@ class FeedbacksController < ApplicationController
       evaluators.append(evaluator.evaluator)
     end
     # TODO: need to properly handle this!!!
-    feedback_template = SurveyTemplate.all()[0]
+    feedback_template = SurveyTemplate.all[0]
     render locals: {
-             advisers: [team.adviser],
-             evaluators: evaluators,
-             feedback_template: feedback_template
-           }
+      advisers: [team.adviser],
+      evaluators: evaluators,
+      feedback_template: feedback_template
+    }
   end
 
   def create
     team = Team.find(params[:team_id])
-    not authenticate_user(true, false, team.get_relevant_users(false, false)) and return
+    !authenticate_user(true, false, team.get_relevant_users(false, false)) && return
     if create_or_update_feedback_and_responses
       redirect_to home_path, flash: {
         success: t('.success_message')
@@ -42,7 +47,7 @@ class FeedbacksController < ApplicationController
       evaluators.append(evaluator.evaluator)
     end
     # TODO: need to properly handle this!!!
-    feedback_template = SurveyTemplate.all()[0]
+    feedback_template = SurveyTemplate.all[0]
     render locals: {
       advisers: [team.adviser],
       evaluators: evaluators,
@@ -52,7 +57,7 @@ class FeedbacksController < ApplicationController
 
   def update
     team = Team.find(params[:team_id])
-    not authenticate_user(true, false, team.get_relevant_users(false, false)) and return
+    !authenticate_user(true, false, team.get_relevant_users(false, false)) && return
     @feedback = Feedback.find(params[:id])
     if create_or_update_feedback_and_responses(@feedback)
       redirect_to home_path, flash: {
@@ -68,38 +73,36 @@ class FeedbacksController < ApplicationController
 
   private
 
-  def get_feedback_params
-    feedback_params = params.require(:feedback).permit!
-    feedback_params[:team_id] = params[:team_id]
-    if feedback_params[:feedback_to_team] == 'on'
-      feedback_params.except!(:adviser_id)
+  def feedback_params
+    feedback_ps = params.require(:feedback).permit!
+    feedback_ps[:team_id] = params[:team_id]
+    if feedback_ps[:feedback_to_team] == 'on'
+      feedback_ps.except!(:adviser_id)
     else
-      feedback_params.except!(:target_team_id)
+      feedback_ps.except!(:target_team_id)
     end
-    if feedback_params[:edit] == 'on'
-      feedback_params = {}
-    end
-    feedback_params.except!(:feedback_to_team)
-    feedback_params
+    feedback_ps = {} if feedback_ps[:edit] == 'on'
+    feedback_ps.except!(:feedback_to_team)
+    feedback_ps
   end
 
-  def get_questions_params
-    questions_params = params.require(:questions).permit!
+  def questions_params
+    params.require(:questions).permit!
   end
 
   def create_or_update_feedback_and_responses(feedback = nil)
     if feedback.nil?
-      @feedback = Feedback.new(get_feedback_params)
-      feedback_template = SurveyTemplate.all()[0]
+      @feedback = Feedback.new(feedback_params)
+      feedback_template = SurveyTemplate.all[0]
       @feedback.survey_template_id = feedback_template.id
     else
-      feedback.update(get_feedback_params)
+      feedback.update(feedback_params)
       @feedback = feedback
     end
-    @feedback.response_content = get_questions_params.to_json
-    if not @feedback.target_team_id.nil?
+    @feedback.response_content = questions_params.to_json
+    if !@feedback.target_team_id.nil?
       @feedback.target_type = Feedback.target_types[:target_type_team]
-    elsif not @feedback.adviser_id.nil?
+    elsif !@feedback.adviser_id.nil?
       @feedback.target_type = Feedback.target_types[:target_type_adviser]
     end
     @feedback.save ? @feedback : nil
