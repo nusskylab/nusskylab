236c22e7fcb05ffba49155a18ef6fb0e97df0533
franklingu <franklingujunchao@gmail.com>
2016-01-13 23:39:42 +0800

    migration for PeerEvaluation2
    noted that some dirty data were found but it seems they belong to
    dropped out teams. may not be important

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112153830) do
+ActiveRecord::Schema.define(version: 20160113145302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
