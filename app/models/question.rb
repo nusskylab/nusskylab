e387fe05b1fba91f7830d152ab88f9e149864a38
franklingu <franklingujunchao@gmail.com>
2016-02-22 23:05:56 +0800

    added validation for json in question
    Close #342

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 # Question: question modeling
 class Question < ActiveRecord::Base
-  before_save :prefill_json_fields
+  before_validation :prefill_json_fields
+  before_save :validate_json_fields
+
   belongs_to :survey_template
 
   enum question_type: [:question_type_text, :question_type_rich_text,
@@ -10,5 +12,18 @@ class Question < ActiveRecord::Base
   def prefill_json_fields
     self.extras = '[]' if extras.blank?
     self.content = '[]' if content.blank?
+  end
+
+  def validate_json_fields
+    begin
+      JSON.parse(extras)
+    rescue JSON::ParserError
+      errors.add(:extras, 'must be a valid JSON string')
+    end
+    begin
+      JSON.parse(content)
+    rescue JSON::ParserError
+      errors.add(:content, 'must be a valid JSON string')
+    end
   end
 end
