e1e7abca84b069e2d948797321f0ed8969ca740d
franklingu <franklingujunchao@gmail.com>
2016-02-23 22:43:38 +0800

    added staff to public_views
    Close #328

--- app/controllers/public_views/public_projects_controller.rb
+++ app/controllers/public_views/public_projects_controller.rb
@@ -3,9 +3,12 @@ class PublicViews::PublicProjectsController < ApplicationController
   def index
     !authenticate_user(false, false) && return
     @page_title = t('.page_title')
-    teams = Team.order(:project_level).reverse_order.all.select do |team|
-      !team.has_dropped
+    @teams_table = {}
+    all_cohorts.each do |cohort|
+      teams = Team.where(
+        cohort: cohort, has_dropped: false
+      ).order(:project_level).reverse_order
+      @teams_table[cohort] = teams
     end
-    @teams_table = { 2015 => teams, 2014 => [], 2013 => [] }
   end
 end
