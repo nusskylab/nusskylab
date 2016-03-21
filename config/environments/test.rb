ee2c89293fa8a62ea93ae44d3a08668aff4faed8
franklingu <franklingujunchao@gmail.com>
2016-03-21 11:20:54 +0800

    fix test failure caused by mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = false
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
