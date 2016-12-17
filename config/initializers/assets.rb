e65071310ad3879cbb624247d0a5afec5ca316b3
le-minhphuc <leminhphuc10t1@gmail.com>
2016-12-17 18:36:26 +0700

    Pre-compilation of css files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.compile = true
-Rails.application.config.assets.precompile += %w(js|css)
+Rails.application.config.assets.precompile += %w(*.js|*.css)
 
 
