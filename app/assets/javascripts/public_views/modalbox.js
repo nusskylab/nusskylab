77804c0ff12177a0d9b586707771593186d9f143
le-minhphuc <leminhphuc10t1@gmail.com>
2016-12-17 22:09:17 +0700

    Trying inline script due to problem with autoclick function in some js file

--- app/assets/javascripts/public_views/modalbox.js
+++ app/assets/javascripts/public_views/modalbox.js
@@ -1,15 +1,1 @@
-function createModalBox(team_name) {
-	var modal = document.getElementById("modal_".concat(team_name));
-	var button = document.getElementById("button_".concat(team_name));
-	var span = document.getElementsByClassName("close")[0];
-	console.log("Success!");
-	modal.style.display = "block";
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	}
-}
+//Empty for now - trying html with script inline
