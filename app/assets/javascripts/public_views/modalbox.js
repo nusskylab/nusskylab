5a521f75c41bb13462e7295db9c966b2bf89a137
le-minhphuc <leminhphuc10t1@gmail.com>
2016-12-17 21:56:15 +0700

    Test create modal box

--- app/assets/javascripts/public_views/modalbox.js
+++ app/assets/javascripts/public_views/modalbox.js
@@ -2,6 +2,7 @@ function createModalBox(team_name) {
 	var modal = document.getElementById("modal_".concat(team_name));
 	var button = document.getElementById("button_".concat(team_name));
 	var span = document.getElementsByClassName("close")[0];
+	console.log("Success!");
 	modal.style.display = "block";
 	span.onclick = function() {
 		modal.style.display = "none";
