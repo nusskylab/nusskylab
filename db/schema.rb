ceb6a5ad6b25dfbe88e1311ccce5082bbd7a73fb
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-04-25 03:13:55 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424185500) do
+ActiveRecord::Schema.define(version: 20180424190355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -202,6 +202,7 @@ ActiveRecord::Schema.define(version: 20180424185500) do
     t.integer  "invitor_student_id"
     t.integer  "cohort"
     t.string   "slide_link"
+    t.string   "video_link"
   end
 
   add_index "teams", ["adviser_id"], name: "index_teams_on_adviser_id", using: :btree
