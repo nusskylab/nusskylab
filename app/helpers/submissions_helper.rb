9c352cdf79aea290fec303c72127bc436bfd0b8e
Jiachao Lin <lin_jiachao@hotmail.com>
2018-05-24 02:06:24 +0800

    Fix all-teams page view

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,15 @@
 module SubmissionsHelper
+
+  def get_project_class_attribute (project_level)
+    if (project_level == "Vostock")
+      "fade in active"
+    else
+      "fade"
+    end
+  end
+
+  def get_team_button_text (team)
+    "#{team.id + 1} .  #{team.team_name}"
+  end
+
 end
