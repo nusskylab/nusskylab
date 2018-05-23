8d5aa80975c92184c96c457b7bf8a2f3b5fa7e52
Jiachao Lin <lin_jiachao@hotmail.com>
2018-05-24 02:31:25 +0800

    Add helpers for submissions

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,5 +1,31 @@
 module SubmissionsHelper
 
+  def project_level_text (project_level)
+    if project_level == 'vostok'
+      text = 'Vostok'
+    elsif project_level == 'project_gemini'
+      text = 'Project Gemini'
+    elsif project_level == 'apollo_11'
+      text = 'Apollo 11'
+    end
+    text
+  end
+
+  def project_level_header (project_level, no_teams)
+    "#{project_level} - #{no_teams} teams"
+  end
+
+  def image_tag_string (project_level)
+    if project_level == 'vostok'
+      tag_string = "Vostok_Icon.jpg"
+    elsif project_level == 'project_gemini'
+      tag_string = "Project_Gemini_Icon.png"
+    elsif project_level == 'apollo_11'
+      tag_string = "Apollo_11_Icon.png"
+    end
+    tag_string
+  end
+
   def get_project_class_attribute (project_level)
     if (project_level == "Vostock")
       "fade in active"
