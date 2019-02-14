876fc299f76115059762c7bb470fd6d60d43e221
yihui <yihui.chong@ncs.com.sg>
2019-02-14 20:36:02 +0800

    server screipt

---
+++ serverstatus.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+killall ruby
+source ~/.bash_profile
+rbenv shell 2.2.1
+# git checkout master
+# git pull
+bundle install
+RAILS_ENV=production bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake db:migrate:status
+RAILS_ENV=production bundle exec rake assets:precompile
+puma -e production 1> log/console.log 2> log/console_err.log &
+
