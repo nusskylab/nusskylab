f366afe5e7136bdafe67eae9070adfa1a15a8053
kennard123661 <kennard_ng@Outlook.com>
2019-05-08 16:38:06 +0800

    add basic tests to ensure that second student is not deleted

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -218,7 +218,7 @@ RSpec.describe StudentsController, type: :controller do
       end
     end
 
-    context 'user logged in and admin' do
+    context 'user logged in and admin and user does not belong to team' do
       login_admin
       it 'should redirect with success for admin user' do
         user = FactoryGirl.create(:user, email: '2@student.controller.spec', uid: '2.student.controller.spec')
@@ -227,6 +227,23 @@ RSpec.describe StudentsController, type: :controller do
         expect(response).to redirect_to(students_path(cohort: controller.current_cohort))
         expect(flash[:success]).not_to be_nil
       end
+
+      it 'should redirect with success for admin user for deletion of student with team' do
+        adviser1 = FactoryGirl.create(:adviser, user_id: subject.current_user.id)
+        user1 = FactoryGirl.create(:user, email: '3@student.controller.spec', uid: '3.student.controller.spec')
+        user2 = FactoryGirl.create(:user, email: '4@student.controller.spec', uid: '4.student.controller.spec')
+        team1 = FactoryGirl.create(:team, adviser: adviser1, team_name: '2.team.controller.spec')
+        student1 = FactoryGirl.create(:student, user_id: user1.id, team: team1)
+        student2 = FactoryGirl.create(:student, user_id: user2.id, team: team1)
+        delete :destroy, id: student1.id
+        expect(response).to redirect_to(students_path(cohort: controller.current_cohort))
+        
+        # expect the team record to be deleted
+        expect{ team1.reload }.to raise_error ActiveRecord::RecordNotFound
+        # expect the other user to still exist
+        expect(Student.where(id: student2.id)).to exist
+      end
+
     end
   end
 end
