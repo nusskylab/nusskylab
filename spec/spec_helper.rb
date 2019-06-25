d21614da49d0a34009e72a4b0f5ad2f01fd0b70f
kennard123661 <kennard_ng@outlook.com>
2019-06-25 16:29:28 +0800

    remove redundant stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,20 +46,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # clean the FactoryGirl Database between each run
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
