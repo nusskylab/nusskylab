f552ac7e077b43ba955879e753c990cc5bb7b51d
kennard123661 <kennard_ng@outlook.com>
2019-06-30 10:37:19 +0800

    add checker for direct link

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -48,6 +48,20 @@ def is_valid_image_url?(url)
   end
 end
 
+def is_direct_image_link?(url, image_extensions=['jpeg', 'jpg', 'png'])
+  url = URI(url)
+  http = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https')
+  res_header = http.head(url.request_uri)
+  
+  is_image = false
+  for image_extension in image_extensions do
+    header_checker = "image/#{image_extension}"
+    contains_checker = res_header['Content-Type'].to_s.include? header_checker
+    is_image = is_image || contains_checker
+  end
+  return is_image
+end  
+
 def print_dict(dict_to_print)
   dict_to_print.each do |key, value|
     puts key.to_s + ' : ' + value
@@ -60,9 +74,11 @@ def is_google_drive_link?(url)
   puts url[0..google_drive_prefix.length]
 end
 
-
-test_url = "https://drive.google.com/open?id=12gYWVnvY_A0v66hY-9zFQMzoZ-j4Tsqf"
-is_google_drive_link? test_url
+test_url = "https://www.smashbros.com/images/og/link.jpg"
+# test_url = "https://drive.google.com/open?id=12gYWVnvY_A0v66hY-9zFQMzoZ-j4Tsqf"
+# is_google_drive_link? test_url
 # test_url = 'https://drive.google.com/open?id=12gYWVnvY_A0v66hY-9zFQMzoZ-j4Tsqf'
-test_url = 'https://drive.google.com/uc?id=12gYWVnvY_A0v66hY-9zFQMzoZ-j4Tsqf&export=view'
-is_valid_image_url?(test_url)
+# test_url = 'https://drive.google.com/uc?id=12gYWVnvY_A0v66hY-9zFQMzoZ-j4Tsqf&export=view'
+# is_valid_image_url?(test_url)
+
+is_direct_image_link? test_url  
