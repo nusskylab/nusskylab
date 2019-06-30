dd586356be575d37f7984b8b34b6b05a7230b433
kennard123661 <kennard_ng@outlook.com>
2019-06-30 12:10:03 +0800

    remove old image link checker

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -3,51 +3,6 @@ require 'net/http'
 require 'uri'
 require 'open-uri'
 
-def is_valid_image_url?(url)
-  og_url = url
-  puts url
-  puts "\n"
-  url = URI(url)
-  # url = URI.parse(url)
-  # http = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https')
-  # out =  http.head(url.request_uri)
-  # out =  get(url)
-  # out = http.get_response(url)
-  # contents = open(or_url) {|f| f.read }
-  # puts contents
-  out = Net::HTTP.get(url)
-  http = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https')
-  out = http.head(url.request_uri)
-  puts out['Content-Type']
-  puts "yeeta skeeta"
-  print_dict(out)
-  # puts out.body
-  if out['Content-Type'].to_s.include? 'text/html' then
-    puts "YEEEEEEEEEEEEEEEEEEEEEEEEEt"
-    raw_url = out['location']
-    puts raw_url
-    url = URI(raw_url)
-    http = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https')
-    out = http.head(url.request_uri)
-    print_dict(out)
-    out = Net::HTTP.get(url)
-    print out
-    # out = Net::HTTP.get_response(url)
-    # print_dict(out)
-    # puts out
-    # out = Net::HTTP.get(url)
-    
-    # puts out
-    # out.each do |key, value|
-      # puts key.to_s + ':' + value
-    # end
-    # out = http.get_response(url)
-    # puts out
-    # print_dict(out)
-    # print_dict(out)
-  end
-end
-
 def is_direct_image_link?(url, image_extensions=['jpeg', 'jpg', 'png'])
   url = URI(url)
   http = Net::HTTP.start(url.host, url.port, :use_ssl => url.scheme == 'https')
