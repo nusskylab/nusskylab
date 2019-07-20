9ec850a60a47aa859744d88d04f762420998cb82
Koh Jun Wei <kjunwei1996@gmail.com>
2019-07-20 11:45:19 +0800

    Add documentation to Config model

--- app/models/config.rb
+++ app/models/config.rb
@@ -1,3 +1,19 @@
+# Project level Configuration
+# To add more project level configurations please follow the following steps:
+#   1. Add your configuration name to STATUS hash.
+#   2. Add the string values that your configuration has.
+#   3. Define getter and setter methods for your configuration.
+#
+# Implementation:
+#   The Config model is implemented with 2 columns in the database table.
+#   1 representing the name, another representing the value of the configuration.
+#
+# Usage:
+#   The Config model must be used from it's declared methods.
+#   Meaning, you should not do things like Config.create/Config.new... etc
+#   outside of the Config model itself.
+#   Hence, if you want to add more configuration, and the current model methods
+#   doesn't satisfy your requirement, feel free to add more methods.
 class Config < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
   validates :value, presence: true
