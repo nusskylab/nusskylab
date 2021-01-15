865d490f0a617c50e419695e8541f2f4693ac159
whitesnowhahorha <yunweibai@gmail.com>
2021-01-15 09:07:54 +0800

    database: update is_pending default

--- db/migrate/20160208132110_add_pending_to_students_and_teams.rb
+++ db/migrate/20160208132110_add_pending_to_students_and_teams.rb
@@ -1,11 +1,11 @@
 class AddPendingToStudentsAndTeams < ActiveRecord::Migration
   def up
-    add_column :students, :is_pending, :boolean, default: false
-    add_column :teams, :is_pending, :boolean, default: false
+    add_column :students, :is_pending, :boolean, default: true
+    add_column :teams, :is_pending, :boolean, default: true
   end
 
   def down
-    remove_column :students, :is_pending, :boolean, default: false
-    remove_column :teams, :is_pending, :boolean, default: false
+    remove_column :students, :is_pending, :boolean, default: true
+    remove_column :teams, :is_pending, :boolean, default: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -172,7 +172,7 @@ ActiveRecord::Schema.define(version: 20200404104546) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "team_id"
-    t.boolean  "is_pending", default: false
+    t.boolean  "is_pending", default: true
     t.integer  "cohort"
   end
 
@@ -215,7 +215,7 @@ ActiveRecord::Schema.define(version: 20200404104546) do
     t.string   "team_name"
     t.boolean  "has_dropped",        default: false
     t.integer  "project_level",      default: 0
-    t.boolean  "is_pending",         default: false
+    t.boolean  "is_pending",         default: true
     t.integer  "invitor_student_id"
     t.integer  "cohort"
     t.string   "poster_link"
