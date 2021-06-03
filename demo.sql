b86bd55a59e56b693422c941597225364a747d27
whitesnowhahorha <yunweibai@gmail.com>
2021-06-03 14:34:20 +0800

    rearrange folder

--- demo.sql
+++
@@ -1,5 +0,0 @@
-UPDATE application_deadlines
-SET submission_deadline = '2022-05-28 09:44:00';
-UPDATE application_deadlines
-SET submission_deadline = '2020-05-28 09:44:00'
-WHERE name = 'form team deadline';
--- reset_deadlines.sql
+++
@@ -1,2 +0,0 @@
-UPDATE application_deadlines
-SET submission_deadline = '2022-05-28 09:44:00',
